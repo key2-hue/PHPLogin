5e7bd5b32a385bc65c3ec9c5bc38d737ef283b43
key2-hue <keymaeda3782@gmail.com>
2020-03-21 08:28:36 +0900

    03/21 08:28

--- next/Controller/Delete.php
+++ next/Controller/Delete.php
@@ -5,6 +5,13 @@ namespace MyApp\Controller;
 class Delete extends \MyApp\Controller {
   public function begin($deleteUser) {
     $this->deleteUser($deleteUser);
+    $_SESSION = [];
+
+    if(isset($_COOKIE[session_name()])) {
+      setcookie(session_name(), '', time() - 86400, '/');
+    }
+
+    session_destroy();
   }
   
 }
